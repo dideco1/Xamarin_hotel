45b39298005ca82890530c96a50284a423dae8ca
dideco1 <diegobaro.si@outlook.com>
2022-09-01 14:36:14 -0300

    Adicionar arquivos de projeto.

---
+++ Xamarin_hotel/Xamarin_hotel/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+using System.Threading;
+using System.Globalization;
+using Xamarin_hotel.Model;
+using Xamarin_hotel.View;
+
+namespace Xamarin_hotel
+{
+    public partial class App : Application
+    {
+        internal List<Suite> Suites = new List<Suite>
+        {
+            new Suite()
+            {
+                Descricao = "Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Suite()
+            {
+                Descricao = "Executiva",
+                ValorDiariaAdulto = 90.0,
+                ValorDiariaCrianca = 45.0
+            },
+            new Suite()
+            {
+                Descricao = "Pobre",
+                ValorDiariaAdulto = 45.0,
+                ValorDiariaCrianca = 20.0
+            }
+        };
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ContratacaoHospedagem());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Xamarin_hotel/Xamarin_hotel/Model/Suite.cs
@@ -0,0 +1,13 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_hotel.Model
+{
+    internal class Suite
+    {
+        public string Descricao { get; set; }
+        public double ValorDiariaAdulto { get; set; }
+        public double ValorDiariaCrianca { get; set; }
+    }
+}
